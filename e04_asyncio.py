8cc7bd3073c363d71365c848c486a43452538e15
kevin <xuanji00@gmail.com>
2017-09-26 16:26:13 +0800

    update asyncio

--- e04_asyncio.py
+++ e04_asyncio.py
@@ -10,7 +10,7 @@ import threading
 @asyncio.coroutine
 def hello():
     print("Hello world!")
-    # 异步调用asyncio.sleep(1):
+    # 直接返回，并异步调用asyncio.sleep(1)，执行完后再返回
     r = yield from asyncio.sleep(1)
     print("Hello again!")
 
@@ -21,16 +21,28 @@ def hello1():
     print('Hello again! (%s)' % threading.currentThread())
 
 
+def test_hello(loop):
+    # 执行coroutine
+    loop.run_until_complete(hello())
+
+    tasks = [hello1(), hello1()]
+    loop.run_until_complete(asyncio.wait(tasks))
+
+
+# wget
 @asyncio.coroutine
 def wget(host):
     print('wget %s...' % host)
+
     connect = asyncio.open_connection(host, 80)
-    reader, writer = yield from connect
+    reader, writer = yield from connect     # await 连接
+    
     header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
     writer.write(header.encode('utf-8'))
-    yield from writer.drain()
+    yield from writer.drain()               # await 发送请求
+    
     while True:
-        line = yield from reader.readline()
+        line = yield from reader.readline() # await 读取响应
         if line == b'\r\n':
             break
         print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
@@ -38,20 +50,21 @@ def wget(host):
     writer.close()
 
 
-# 获取EventLoop:
-loop = asyncio.get_event_loop()
+def test_wget():
+    print("\n========== test_wget ============")
 
-# 1. 执行coroutine
-loop.run_until_complete(hello())
+    tasks = [wget(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
+    loop.run_until_complete(asyncio.wait(tasks))
 
-# 2.
-tasks = [hello1(), hello1()]
-loop.run_until_complete(asyncio.wait(tasks))
 
-# 3.
-tasks = [wget(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
-loop.run_until_complete(asyncio.wait(tasks))
 
-loop.close()
 
+if __name__=='__main__':
+    # 获取EventLoop:
+    loop = asyncio.get_event_loop()
+
+    test_hello(loop)
+    test_wget()
+
+    loop.close()
 
